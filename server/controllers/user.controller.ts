bead0ae8ca44f34796c366c065cd9917e12ffc85
Donka <DoniGeorgieva16@gmail.com>
2023-10-07 10:44:50 +0300

    sign in and sign up users

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -1,6 +1,60 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import UserModel from "../mongodb/models/user";
+import bcrypt from "bcrypt";
+import * as jwt from "jsonwebtoken";
+const signUp = async (req: Request, res: Response) => {
+  const { email, password, confirmPassword, username } = req.body;
+  try {
+    const userExists = await UserModel.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    if (password !== confirmPassword) {
+      return res.status(400).json({ message: "Passwords don't match" });
+    }
 
-const getUser = async (req: express.Request, res: express.Response) => {};
-const createUser = async (req: express.Request, res: express.Response) => {};
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const result = await UserModel.create({
+      email,
+      username,
+      password: hashedPassword,
+    });
+    const token = jwt.sign(
+      { email: result.email, id: result._id },
+      `${process.env.SECRET}`,
+      { expiresIn: "1h" }
+    );
+    res
+      .status(200)
+      .json({ email: result.email, username: result.username, token });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong!" });
+  }
+};
+const signIn = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  try {
+    const existingUser = await UserModel.findOne({ email });
+    if (!existingUser) {
+      return res.status(404).json({ message: "User doesn't exist" });
+    }
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
 
-export { createUser, getUser };
+    const token = jwt.sign(
+      { email: existingUser.email, id: existingUser._id },
+      `${process.env.SECRET}`,
+      { expiresIn: "1h" }
+    );
+    res.status(200).json(token);
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong!" });
+  }
+};
+
+export { signUp, signIn };
--- server/mongodb/models/user.ts
+++ server/mongodb/models/user.ts
@@ -3,7 +3,8 @@ import mongoose, { Model, Schema } from "mongoose";
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
-  avatar: { type: String },
+  password: { type: String, required: true },
+  _id: { type: mongoose.Schema.Types.ObjectId },
   savedAPIs: [{ type: mongoose.Schema.Types.ObjectId, ref: "Savedapi" }],
 });
 
